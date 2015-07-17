59afbe35548128d36637292cd6028663161ba804
mitkaua <mitkaua@gmail.com>
2015-07-17 17:25:49 +0300

    filters
    add filter

--- song/views.py
+++ song/views.py
@@ -5,4 +5,12 @@ from models import Song
 
 def song_list(request):
     songs = Song.objects.all()
+    songs = songs.order_by('artist_name')
+    if request.GET.get('order_by', '') == '':
+        request.GET.order_by = 'artist_name'
+    order_by = request.GET.get('order_by', '')
+    if order_by in ('artist_name', 'composition_name', 'id'):
+        songs = songs.order_by(order_by)
+        if request.GET.get('reverse', '') == '1':
+            songs = songs.reverse()
     return render(request, 'song/base.html', {'songs': songs})
